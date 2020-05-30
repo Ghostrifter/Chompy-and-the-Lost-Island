f6beded5f3e0c209c5fca2c2b27c1a00fcd59e09
Ghostrifter <60340180+Ghostrifter@users.noreply.github.com>
2020-05-30 16:14:40 +0200

    Enemy Waypoint AI

---
+++ Chompy And The Lost Island/Assets/Scripts/BeetleAI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BeetleAI : MonoBehaviour
+{
+    public float speed;
+    private float waitTime;
+    public float startWaitTime;
+
+    public Transform[] moveSpots;
+    private int randomSpot;
+
+    void Start()
+    {
+        randomSpot = Random.Range(0, moveSpots.Length);
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, moveSpots[randomSpot].position) <0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                randomSpot = Random.Range(0, moveSpots.Length);
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+}
